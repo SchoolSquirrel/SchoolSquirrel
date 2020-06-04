2911337148d61f804f4b64d9ca02211a3e0e5349
Hannes Rüger <hannesrueger@gmx.de>
2020-06-04 13:47:39 +0200

    added docker build scripts

---
+++ docker/build.sh
@@ -0,0 +1,10 @@
+cd ../SchoolSquirrel
+#npm install
+#npm run build -- --outputPath=../docker/frontend
+cd ../api
+#npm install
+#npx @zeit/ncc build src/index.ts -o ../docker/backend
+cd ../docker
+PACKAGE_VERSION=$(cat ../api/package.json | grep version | head -1 | awk -F: '{ print $2 }' | sed 's/[",]//g' | tr -d '[[:space:]]')
+docker build -t schoolsquirrel/schoolsquirrel:v$PACKAGE_VERSION .
+rm -r frontend backend
