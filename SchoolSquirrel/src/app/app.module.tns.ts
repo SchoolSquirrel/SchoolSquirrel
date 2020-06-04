45cccac37a521bb6557d7ea21c34cbab5bd458ca
Hannes Rüger <hannesrueger@gmx.de>
2020-06-04 10:11:43 +0200

    migrated to scoped nativescript angular package

--- SchoolSquirrel/src/app/app.module.tns.ts
+++ SchoolSquirrel/src/app/app.module.tns.ts
@@ -1,8 +1,7 @@
 import { NgModule, NO_ERRORS_SCHEMA } from "@angular/core";
-import { NativeScriptModule } from "nativescript-angular/nativescript.module";
-
-import { NativeScriptFormsModule } from "nativescript-angular/forms";
-import { NativeScriptHttpClientModule } from "nativescript-angular/http-client";
+import { NativeScriptModule } from "@nativescript/angular/nativescript.module";
+import { NativeScriptFormsModule } from "@nativescript/angular/forms";
+import { NativeScriptHttpClientModule } from "@nativescript/angular/http-client";
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 import { HomeComponent } from "./_pages/home/home.component";
