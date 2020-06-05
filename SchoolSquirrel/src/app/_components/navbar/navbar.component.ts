c91b8f4c5a4d659e6345be807beaeb317d86c8f2
Hannes Rüger <hannesrueger@gmx.de>
2020-06-05 14:33:49 +0200

    added command / search base

--- SchoolSquirrel/src/app/_components/navbar/navbar.component.ts
+++ SchoolSquirrel/src/app/_components/navbar/navbar.component.ts
@@ -1,4 +1,24 @@
 import { Component } from "@angular/core";
+import { debounceTime, map } from "rxjs/operators";
+import { Observable } from "rxjs";
+
+const actions: {
+    name: string;
+    description: string;
+}[] = [
+    {
+        name: "Call...",
+        description: "Call a user",
+    },
+    {
+        name: "Offline",
+        description: "Set your status to offline",
+    },
+    {
+        name: "Online",
+        description: "Set your status to online",
+    },
+];
 
 @Component({
     selector: "app-navbar",
@@ -6,5 +26,23 @@ import { Component } from "@angular/core";
     styleUrls: ["./navbar.component.scss"],
 })
 export class NavbarComponent {
-    //
+    public actionValue = "";
+    public inputFocused = false;
+
+    public openTypeahead(element: HTMLInputElement): void {
+        element.value = "";
+        element.dispatchEvent(new Event("input"));
+        element.focus();
+    }
+
+    public action(text$: Observable<string>): Observable<any[]> {
+        return text$.pipe(
+            debounceTime(200),
+            map((term) => (term === ""
+                ? actions
+                : actions.filter(
+                    (v) => v.name.toLowerCase().indexOf(term.toLowerCase()) > -1,
+                ).slice(0, 10))),
+        );
+    }
 }
