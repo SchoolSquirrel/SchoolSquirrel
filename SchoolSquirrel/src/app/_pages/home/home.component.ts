74e91a01a7fddc7d3b75ef67cb54d25a177f3081
Hannes Rüger <hannesrueger@gmx.de>
2020-06-05 15:25:17 +0200

    added welcome message

--- SchoolSquirrel/src/app/_pages/home/home.component.ts
+++ SchoolSquirrel/src/app/_pages/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component } from "@angular/core";
+import { AuthenticationService } from "../../_services/authentication.service";
 
 @Component({
     selector: "app-home",
@@ -6,4 +7,5 @@ import { Component } from "@angular/core";
     styleUrls: ["./home.component.scss"],
 })
 export class HomeComponent {
+    constructor(public authenticationService: AuthenticationService) {}
 }
