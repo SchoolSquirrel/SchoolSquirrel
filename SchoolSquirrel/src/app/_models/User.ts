ab231a2c028adba2a4c418d33dd535f215eef2b7
Hannes Rüger <hannesrueger@gmx.de>
2020-06-06 13:11:05 +0200

    i18n for all text

--- SchoolSquirrel/src/app/_models/User.ts
+++ SchoolSquirrel/src/app/_models/User.ts
@@ -6,38 +6,38 @@
 /*    |    Then, run "npx redundancyjs" in the terminal.                      |    */
 /*    +-----------------------------------------------------------------------+    */
 
+/* do not edit */ 
+/* do not edit */ 
 /* do not edit */
-/* do not edit */
-/* do not edit */
-/* do not edit */
-/* do not edit */
+/* do not edit */ 
+/* do not edit */ 
 /* do not edit */ export class User {
+/* do not edit */   
+/* do not edit */   public id: number;
 /* do not edit */
-/* do not edit */ public id: number;
-    /* do not edit */
-    /* do not edit */
-    /* do not edit */ public username: string;
-    /* do not edit */
-    /* do not edit */
-    /* do not edit */ public isAdmin: boolean;
-    /* do not edit */
-    /* do not edit */
-    /* do not edit */ public password: string;
-    /* do not edit */
-    /* do not edit */
-    /* do not edit */ public passwordResetToken: string;
-    /* do not edit */
-    /* do not edit */
-    /* do not edit */
-    /* do not edit */ public createdAt: Date;
-    /* do not edit */
-    /* do not edit */
-    /* do not edit */
-    /* do not edit */ public updatedAt: Date;
-    /* do not edit */
-    /* do not edit */
-    /* do not edit */
-    /* do not edit */ public jwtToken?: string;
+/* do not edit */   
+/* do not edit */   public username: string;
 /* do not edit */
+/* do not edit */   
+/* do not edit */   public isAdmin: boolean;
 /* do not edit */
+/* do not edit */   
+/* do not edit */   public password: string;
+/* do not edit */
+/* do not edit */   
+/* do not edit */   public passwordResetToken: string;
+/* do not edit */
+/* do not edit */   
+/* do not edit */   
+/* do not edit */   public createdAt: Date;
+/* do not edit */
+/* do not edit */   
+/* do not edit */   
+/* do not edit */   public updatedAt: Date;
+/* do not edit */
+/* do not edit */   
+/* do not edit */
+/* do not edit */   public jwtToken?: string;
+/* do not edit */
+/* do not edit */   
 /* do not edit */ }
