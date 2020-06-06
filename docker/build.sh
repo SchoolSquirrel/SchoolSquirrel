aee58a7b855230d7b34b63c66efe82dfa7baf3c0
Hannes Rüger <hannesrueger@gmx.de>
2020-06-06 14:49:56 +0200

    fixed docker build script

--- docker/build.sh
+++ docker/build.sh
@@ -1,9 +1,9 @@
 cd ../SchoolSquirrel
-#npm install
-#npm run build -- --outputPath=../docker/frontend
+npm install
+npm run build -- --outputPath=../docker/frontend
 cd ../api
-#npm install
-#npx @zeit/ncc build src/index.ts -o ../docker/backend
+npm install
+npx @zeit/ncc build src/index.ts -o ../docker/backend
 cd ../docker
 PACKAGE_VERSION=$(cat ../api/package.json | grep version | head -1 | awk -F: '{ print $2 }' | sed 's/[",]//g' | tr -d '[[:space:]]')
 docker build -t schoolsquirrel/schoolsquirrel:v$PACKAGE_VERSION .
