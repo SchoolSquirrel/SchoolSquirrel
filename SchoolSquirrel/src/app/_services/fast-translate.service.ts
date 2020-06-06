13b368b28e751725724ee65b9ce060b7a558dee2
Hannes Rüger <hannesrueger@gmx.de>
2020-06-06 13:10:50 +0200

    added FastTranslateService

---
+++ SchoolSquirrel/src/app/_services/fast-translate.service.ts
@@ -0,0 +1,11 @@
+import { TranslateService } from "@ngx-translate/core";
+import { Injectable } from "@angular/core";
+
+@Injectable({ providedIn: "root" })
+export class FastTranslateService {
+    constructor(private translateService: TranslateService) { }
+
+    public async t(key: string): Promise<string> {
+        return this.translateService.get(key).toPromise();
+    }
+}
