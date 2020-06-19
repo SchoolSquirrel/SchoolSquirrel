b75bf01e1b6f06fdeadcbf7a579daeede074d6af
Hannes Rüger <hannesrueger@gmx.de>
2020-06-19 11:14:24 +0200

    tried to make login

--- SchoolSquirrel/src/app/app.module.tns.ts
+++ SchoolSquirrel/src/app/app.module.tns.ts
@@ -1,5 +1,6 @@
 import { NgModule, NO_ERRORS_SCHEMA, CUSTOM_ELEMENTS_SCHEMA } from "@angular/core";
 import { NativeScriptModule, NativeScriptFormsModule, NativeScriptHttpClientModule } from "@nativescript/angular";
+import { ReactiveFormsModule } from "@angular/forms";
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 import { HomeComponent } from "./_pages/home/home.component";
@@ -12,6 +13,7 @@ import { LoginComponent } from "./_pages/login/login.component";
         LoginComponent,
     ],
     imports: [
+        ReactiveFormsModule,
         NativeScriptModule,
         AppRoutingModule,
         NativeScriptFormsModule,
