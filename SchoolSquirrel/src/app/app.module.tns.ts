763794374dedd8e4d8869ff9c4919a756c9411a1
Hannes Rüger <hannesrueger@gmx.de>
2020-06-19 13:54:05 +0200

    disabled ivy

--- SchoolSquirrel/src/app/app.module.tns.ts
+++ SchoolSquirrel/src/app/app.module.tns.ts
@@ -1,16 +1,31 @@
 import { NgModule, NO_ERRORS_SCHEMA, CUSTOM_ELEMENTS_SCHEMA } from "@angular/core";
 import { NativeScriptModule, NativeScriptFormsModule, NativeScriptHttpClientModule } from "@nativescript/angular";
 import { ReactiveFormsModule } from "@angular/forms";
+import { TranslateModule } from "@ngx-translate/core";
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 import { HomeComponent } from "./_pages/home/home.component";
 import { LoginComponent } from "./_pages/login/login.component";
+import { AssignmentsComponent } from "./_pages/assignments/assignments.component";
+import { CalendarComponent } from "./_pages/calendar/calendar.component";
+import { UsersComponent } from "./_pages/_admin/users/users.component";
+import { SettingsComponent } from "./_pages/_admin/settings/settings.component";
+import { NavbarComponent } from "./_components/navbar/navbar.component";
+import { SidebarComponent } from "./_components/sidebar/sidebar.component";
+import { ToastComponent } from "./_components/toast/toast.component";
 
 @NgModule({
     declarations: [
         AppComponent,
         HomeComponent,
         LoginComponent,
+        ToastComponent,
+        SidebarComponent,
+        NavbarComponent,
+        AssignmentsComponent,
+        CalendarComponent,
+        UsersComponent,
+        SettingsComponent,
     ],
     imports: [
         ReactiveFormsModule,
@@ -18,6 +33,7 @@ import { LoginComponent } from "./_pages/login/login.component";
         AppRoutingModule,
         NativeScriptFormsModule,
         NativeScriptHttpClientModule,
+        TranslateModule.forRoot(),
     ],
     providers: [],
     bootstrap: [AppComponent],
