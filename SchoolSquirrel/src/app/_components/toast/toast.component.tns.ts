5c080dc71848f0de4735d6bcfe98d18ac746ff07
Hannes Rüger <hannesrueger@gmx.de>
2020-06-25 20:03:57 +0200

    fixed toasts on mobile

---
+++ SchoolSquirrel/src/app/_components/toast/toast.component.tns.ts
@@ -0,0 +1,7 @@
+import { Component } from "@angular/core";
+@Component({
+    selector: "app-toasts",
+    templateUrl: "./toast.component.html",
+    styleUrls: ["./toast.component.scss"],
+})
+export class ToastComponent {}
