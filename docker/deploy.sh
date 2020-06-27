557e31f79b668f326c47cabf3588ba13fe2b76ef
Hannes Rüger <hannesrueger@gmx.de>
2020-06-27 11:11:13 +0200

    fixed docker build tag does not exist

--- docker/deploy.sh
+++ docker/deploy.sh
@@ -1,3 +1,3 @@
-PACKAGE_VERSION=$(cat ../package.json | grep version | head -1 | awk -F: '{ print $2 }' | sed 's/[",]//g' | tr -d '[[:space:]]')
+PACKAGE_VERSION=$(cat ../SchoolSquirrel/package.json | grep version | head -1 | awk -F: '{ print $2 }' | sed 's/[",]//g' | tr -d '[[:space:]]')
 docker login -u $1 -p $2
 docker push schoolsquirrel/schoolsquirrel:v$PACKAGE_VERSION
