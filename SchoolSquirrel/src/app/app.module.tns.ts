257f1a45e0064f150b1dd582e8409ca84aa5afa5
Hannes Rüger <hannesrueger@gmx.de>
2020-06-27 12:01:25 +0200

    (mobile) added error and jwt interceptors

--- SchoolSquirrel/src/app/app.module.tns.ts
+++ SchoolSquirrel/src/app/app.module.tns.ts
@@ -1,8 +1,11 @@
-import { NgModule, NO_ERRORS_SCHEMA, CUSTOM_ELEMENTS_SCHEMA } from "@angular/core";
+import {
+    NgModule, NO_ERRORS_SCHEMA, CUSTOM_ELEMENTS_SCHEMA, LOCALE_ID,
+} from "@angular/core";
 import { NativeScriptModule, NativeScriptFormsModule, NativeScriptHttpClientModule } from "@nativescript/angular";
 import { ReactiveFormsModule } from "@angular/forms";
 import { TranslateModule, TranslateLoader } from "@ngx-translate/core";
 import { NativeScriptLoader } from "@danvick/ngx-translate-nativescript-loader";
+import { HTTP_INTERCEPTORS } from "@angular/common/http";
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 import { HomeComponent } from "./_pages/home/home.component";
@@ -19,6 +22,8 @@ import { SelectUsersComponent } from "./_components/select-users/select-users.co
 import { CourseComponent } from "./_pages/course/course.component";
 import { FullPageLoadingComponent } from "./_components/full-page-loading/full-page-loading.component";
 import { ChatComponent } from "./_pages/chat/chat.component";
+import { JwtInterceptor } from "./_interceptors/jwt.interceptor";
+import { ErrorInterceptor } from "./_interceptors/error.interceptor";
 
 export function nativescriptTranslateLoaderFactory(): NativeScriptLoader {
     return new NativeScriptLoader("./assets/i18n/", ".json");
@@ -56,7 +61,19 @@ export function nativescriptTranslateLoaderFactory(): NativeScriptLoader {
             defaultLanguage: "de",
         }),
     ],
-    providers: [],
+    providers: [
+        {
+            provide: HTTP_INTERCEPTORS,
+            useClass: ErrorInterceptor,
+            multi: true,
+        },
+        {
+            provide: HTTP_INTERCEPTORS,
+            useClass: JwtInterceptor,
+            multi: true,
+        },
+        { provide: LOCALE_ID, useValue: "de-DE" },
+    ],
     bootstrap: [AppComponent],
     schemas: [NO_ERRORS_SCHEMA, CUSTOM_ELEMENTS_SCHEMA],
 })
