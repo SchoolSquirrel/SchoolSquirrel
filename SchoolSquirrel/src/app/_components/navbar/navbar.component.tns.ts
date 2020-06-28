c9912d9682ffce4b163d2002f940abe62e136b6d
Hannes Rüger <hannesrueger@gmx.de>
2020-06-28 13:16:53 +0200

    added navbar

---
+++ SchoolSquirrel/src/app/_components/navbar/navbar.component.tns.ts
@@ -0,0 +1,17 @@
+import { Component, Input } from "@angular/core";
+import { Application } from "@nativescript/core";
+import { RadSideDrawer } from "nativescript-ui-sidedrawer";
+
+@Component({
+    selector: "app-navbar",
+    templateUrl: "./navbar.component.html",
+    styleUrls: ["./navbar.component.scss"],
+})
+export class NavbarComponent {
+    @Input() public title: string;
+    @Input() public isModal: boolean;
+
+    public openSideDrawer(): void {
+        (Application.getRootView() as any as RadSideDrawer).showDrawer();
+    }
+}
