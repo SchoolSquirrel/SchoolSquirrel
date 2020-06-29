83eb79b2f6d0bba176027d3b7b50d1903cc53722
Hannes Rüger <hannesrueger@gmx.de>
2020-06-29 13:42:04 +0200

    (web) list users in navbar action menu

--- SchoolSquirrel/src/app/_pages/home/home.component.ts
+++ SchoolSquirrel/src/app/_pages/home/home.component.ts
@@ -1,5 +1,8 @@
 import { Component } from "@angular/core";
 import { AuthenticationService } from "../../_services/authentication.service";
+import { RemoteService } from "../../_services/remote.service";
+import { NavbarAction } from "../../_services/NavbarAction";
+import { User } from "../../_models/User";
 
 @Component({
     selector: "app-home",
@@ -7,5 +10,22 @@ import { AuthenticationService } from "../../_services/authentication.service";
     styleUrls: ["./home.component.scss"],
 })
 export class HomeComponent {
-    constructor(public authenticationService: AuthenticationService) {}
+    constructor(
+        public authenticationService: AuthenticationService,
+        private remoteService: RemoteService,
+    ) { }
+
+    public ngOnInit(): void {
+        this.remoteService.get("users").subscribe((users: User[]) => {
+            for (const user of users) {
+                (window as any).actions.push({
+                    description: `Chat with ${user.username}`,
+                    name: user.username,
+                    subtitle: user.grade?.name,
+                    isUser: true,
+                    img: this.remoteService.getImageUrl(`users/${user.id}.svg`, this.authenticationService),
+                } as NavbarAction);
+            }
+        });
+    }
 }
