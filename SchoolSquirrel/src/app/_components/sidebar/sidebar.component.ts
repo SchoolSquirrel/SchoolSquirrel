9365628f001ed21b1b1c9d0b9446c0af8d2d45a6
Hannes Rüger <hannesrueger@gmx.de>
2020-06-29 11:43:22 +0200

    (web) fixed error loading courses after login

--- SchoolSquirrel/src/app/_components/sidebar/sidebar.component.ts
+++ SchoolSquirrel/src/app/_components/sidebar/sidebar.component.ts
@@ -1,4 +1,4 @@
-import { Component } from "@angular/core";
+import { Component, ChangeDetectorRef } from "@angular/core";
 import { AuthenticationService } from "../../_services/authentication.service";
 import { RemoteService } from "../../_services/remote.service";
 import { Course } from "../../_models/Course";
@@ -13,9 +13,13 @@ export class SidebarComponent {
     constructor(
         public authenticationService: AuthenticationService,
         private remoteService: RemoteService,
+        private cdr: ChangeDetectorRef,
     ) { }
 
-    public ngOnInit(): void {
+    public async ngOnInit(): Promise<void> {
+        while (!this.authenticationService.currentUser) {
+            await new Promise((resolve) => setTimeout(() => resolve(), 100));
+        }
         this.remoteService.get("courses").subscribe((data) => {
             this.courses = data;
         });
