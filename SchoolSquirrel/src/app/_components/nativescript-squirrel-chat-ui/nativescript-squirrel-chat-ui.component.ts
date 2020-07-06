5b35aa70926ded922b9f15e21dce0bf492d4e16e
Hannes Rüger <hannesrueger@gmx.de>
2020-07-06 17:01:09 +0200

    (web) fixed build error

--- SchoolSquirrel/src/app/_components/nativescript-squirrel-chat-ui/nativescript-squirrel-chat-ui.component.ts
+++ SchoolSquirrel/src/app/_components/nativescript-squirrel-chat-ui/nativescript-squirrel-chat-ui.component.ts
@@ -1,21 +1,8 @@
-import { Component, Input } from "@angular/core";
-import { ObservableArray } from "@nativescript/core";
-import { Message } from "../../_models/Message";
-import { User } from "../../_models/User";
+import { Component } from "@angular/core";
 
 @Component({
     selector: "nativescript-squirrel-chat-ui",
     templateUrl: "./nativescript-squirrel-chat-ui.component.html",
     styleUrls: ["./nativescript-squirrel-chat-ui.component.css"],
 })
-export class NativescriptSquirrelChatUiComponent {
-    @Input()
-    set messages(val: Message[]) {
-        this.messages.splice(0, this.messages.length);
-        this._messages.push(...this.messages);
-    }
-    @Input() hideHeader = false;
-    @Input() me: User;
-
-    public _messages: ObservableArray<Message> = new ObservableArray<Message>();
-}
+export class NativescriptSquirrelChatUiComponent {}
