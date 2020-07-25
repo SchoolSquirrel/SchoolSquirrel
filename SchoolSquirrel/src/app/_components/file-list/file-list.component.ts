853f5bf814c3b6d1bd99365c39f4df458d2917b9
Hannes Rüger <hannesrueger@gmx.de>
2020-07-25 17:54:03 +0200

    only display add button when in edit mode

--- SchoolSquirrel/src/app/_components/file-list/file-list.component.ts
+++ SchoolSquirrel/src/app/_components/file-list/file-list.component.ts
@@ -1,4 +1,4 @@
-import { Component } from "@angular/core";
+import { Component, Input } from "@angular/core";
 
 @Component({
     selector: "app-file-list",
@@ -6,5 +6,6 @@ import { Component } from "@angular/core";
     styleUrls: ["./file-list.component.scss"],
 })
 export class FileListComponent {
+  @Input() public edit = false;
   public files: any[] = [];
 }
