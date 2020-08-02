a3320f6606152064a4acfba232add1544869b897
Hannes Rüger <hannesrueger@gmx.de>
2020-08-02 15:46:51 +0200

    (web) (files list) added download logic

--- SchoolSquirrel/src/app/_components/file-list/file-list.component.ts
+++ SchoolSquirrel/src/app/_components/file-list/file-list.component.ts
@@ -2,8 +2,9 @@ import {
     Component, Input, ViewChild, ElementRef,
 } from "@angular/core";
 import { RemoteService } from "../../_services/remote.service";
-import { FilenamePipe } from "@src/app/_pipes/filename.pipe";
-import { FileextPipe } from "@src/app/_pipes/fileext.pipe";
+import { FilenamePipe } from "../../_pipes/filename.pipe";
+import { FileextPipe } from "../../_pipes/fileext.pipe";
+import { AuthenticationService } from "../../_services/authentication.service";
 
 @Component({
     selector: "app-file-list",
@@ -12,29 +13,40 @@ import { FileextPipe } from "@src/app/_pipes/fileext.pipe";
 })
 export class FileListComponent {
     @Input() public edit = false;
-    @Input() public type: "materials" | "worksheets" = "materials";
+    @Input() public type: "materials" | "worksheets";
     @Input() public id: number;
     @Input() public files: any[] = [];
     // eslint-disable-next-line react/static-property-placement
     @Input() public context: string;
     @ViewChild("fileInput") private fileInput: ElementRef;
 
-    constructor(private remoteService: RemoteService) {}
+    constructor(
+        private remoteService: RemoteService,
+        private authenticationService: AuthenticationService,
+    ) { }
 
     public uploadFile(): void {
         this.fileInput.nativeElement.click();
     }
 
-    public delete(file: Record<string, unknown>): void {
+    public delete(file: {name: string}): void {
         // eslint-disable-next-line
         if (!confirm("Soll diese Datei wirklich gelöscht werden?")) {
             return;
         }
-        this.remoteService.delete(`${this.context}/${this.id}/${this.type}/${new FilenamePipe().transform(file.name as string)}.${new FileextPipe().transform(file.name as string)}`).subscribe((data) => {
+        this.remoteService.delete(this.getContextUrl(file)).subscribe((data) => {
             if (data && data.success) {
                 this.files = this.files.filter((f) => f.name !== file.name);
             }
         });
+    }
+
+    public download(file: { name: string }): void {
+        window.open(this.remoteService.getImageUrl(this.getContextUrl(file), this.authenticationService), "_blank");
+    }
+
+    private getContextUrl(file: { name: string; }): string {
+        return `${this.context}/${this.id}/${this.type}/${new FilenamePipe().transform(file.name as string)}.${new FileextPipe().transform(file.name as string)}`;
     }
 
     public onFileSelected(event: Event): void {
