bcb3d3308e8abfb7e9d373f9095937424caefa1d
Hannes Rüger <hannesrueger@gmx.de>
2020-08-12 16:27:13 +0200

    (desktop) (login) added window icons

--- SchoolSquirrel/src/app/_pages/login/login.component.ts
+++ SchoolSquirrel/src/app/_pages/login/login.component.ts
@@ -1,11 +1,13 @@
 import { Component } from "@angular/core";
 import { HttpClient } from "@angular/common/http";
 import { Router, ActivatedRoute } from "@angular/router";
+import { isElectron } from "../../_helpers/isElectron";
 import { LoginComponentCommon } from "./login.component.common";
 import { ToastService } from "../../_services/toast.service";
 import { RemoteService } from "../../_services/remote.service";
 import { AuthenticationService } from "../../_services/authentication.service";
 import { StorageService } from "../../_services/storage.service";
+import { ElectronService } from "../../_services/electron.service";
 
 @Component({
     selector: "app-login",
@@ -13,6 +15,8 @@ import { StorageService } from "../../_services/storage.service";
     styleUrls: ["./login.component.scss"],
 })
 export class LoginComponent extends LoginComponentCommon {
+    public isElectron: boolean = isElectron();
+    public isMaximized = false;
     constructor(
         httpClient: HttpClient,
         toastService: ToastService,
@@ -21,8 +25,31 @@ export class LoginComponent extends LoginComponentCommon {
         router: Router,
         storageService: StorageService,
         route: ActivatedRoute,
+        private electronService: ElectronService,
     ) {
         super(httpClient, toastService, remoteService,
             authenticationService, router, storageService, route);
+    }
+
+    public minWindow(): void {
+        this.electronService.runIfElectron((_, currentWindow) => {
+            currentWindow.minimize();
+        });
+    }
+
+    public maxWindow(): void {
+        this.isMaximized = !this.isMaximized;
+        this.electronService.runIfElectron((_, currentWindow) => {
+            if (currentWindow.isMaximized()) {
+                currentWindow.unmaximize();
+            } else {
+                currentWindow.maximize();
+            }
+        });
+    }
+    public closeWindow(): void {
+        this.electronService.runIfElectron((_, currentWindow) => {
+            currentWindow.hide();
+        });
     }
 }
