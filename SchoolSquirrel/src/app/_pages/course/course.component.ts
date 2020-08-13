a7f35d059d457e4a949f5cab374ac2ec7c434a58
Hannes Rüger <hannesrueger@gmx.de>
2020-08-13 17:09:37 +0200

    (web) (courses) added editing course settings

--- SchoolSquirrel/src/app/_pages/course/course.component.ts
+++ SchoolSquirrel/src/app/_pages/course/course.component.ts
@@ -2,12 +2,14 @@ import { Component, OnInit, ViewChild } from "@angular/core";
 import { ActivatedRoute, Router } from "@angular/router";
 import { L10n, setCulture } from "@syncfusion/ej2-base";
 import { FileOpenEventArgs, FileManagerComponent } from "@syncfusion/ej2-angular-filemanager";
+import { NgbModal } from "@ng-bootstrap/ng-bootstrap";
 import { Course } from "../../_models/Course";
 import { RemoteService } from "../../_services/remote.service";
 import { AuthenticationService } from "../../_services/authentication.service";
 import { Message } from "../../_models/Message";
 import { MessageStatus } from "../../_models/MessageStatus";
 import { FastTranslateService } from "../../_services/fast-translate.service";
+import { CourseConfigComponent } from "../../_dialogs/course-config/course-config.component";
 
 type Tab = "chat" | "files" | "assignments" | "settings";
 
@@ -20,6 +22,7 @@ export class CourseComponent implements OnInit {
     public course: Course;
     public ajaxSettings = {};
     public activeTab: Tab = "chat";
+    public loading = false;
     @ViewChild("filemanager") private filemanager: FileManagerComponent;
 
     constructor(
@@ -27,6 +30,7 @@ export class CourseComponent implements OnInit {
         private remoteService: RemoteService,
         private route: ActivatedRoute,
         private fts: FastTranslateService,
+        private modalService: NgbModal,
         private router: Router,
     ) {
         setCulture("de");
@@ -55,22 +59,27 @@ export class CourseComponent implements OnInit {
             this.activeTab = params.tab || "chat";
             if (parseInt(params.id, 10) != this.course?.id) {
                 this.course = undefined;
-                this.remoteService.get(`courses/${params.id}`).subscribe((data) => {
-                    this.course = data;
-                    for (const message of this.course.messages) {
-                        message.fromMe = message.sender.id
-                            == this.authenticationService.currentUser.id;
-                    }
-                    const prefix = `${this.remoteService.apiUrl}/files/course/${this.course.id}`;
-                    const suffix = `?authorization=${this.authenticationService.currentUser.jwtToken}`;
-                    this.ajaxSettings = {
-                        url: `${prefix}${suffix}`,
-                        downloadUrl: `${prefix}/download`,
-                        uploadUrl: `${prefix}/upload${suffix}`,
-                        getImageUrl: `${prefix}/serve`,
-                    };
-                });
+                this.loadCourse(params.id);
             }
+        });
+    }
+
+    private loadCourse(id: number) {
+        this.remoteService.get(`courses/${id}`).subscribe((data) => {
+            this.loading = false;
+            this.course = data;
+            for (const message of this.course.messages) {
+                message.fromMe = message.sender.id
+                    == this.authenticationService.currentUser.id;
+            }
+            const prefix = `${this.remoteService.apiUrl}/files/course/${this.course.id}`;
+            const suffix = `?authorization=${this.authenticationService.currentUser.jwtToken}`;
+            this.ajaxSettings = {
+                url: `${prefix}${suffix}`,
+                downloadUrl: `${prefix}/download`,
+                uploadUrl: `${prefix}/upload${suffix}`,
+                getImageUrl: `${prefix}/serve`,
+            };
         });
     }
 
@@ -80,5 +89,25 @@ export class CourseComponent implements OnInit {
             this.course.messages[this.course.messages.findIndex((msg) => msg.id == m.id)]
                 .status = MessageStatus.Sent;
         });
+    }
+
+    public configureCourse(): void {
+        const modal = this.modalService.open(CourseConfigComponent);
+        (modal.componentInstance as CourseConfigComponent).mode = "edit";
+        (modal.componentInstance as CourseConfigComponent).name = this.course.name;
+        (modal.componentInstance as CourseConfigComponent).description = this.course.description;
+        (modal.componentInstance as CourseConfigComponent).users = this.course.students;
+        modal.result.then((result) => {
+            this.loading = true;
+            this.remoteService.post(`courses/${this.course.id}`, {
+                name: result.name,
+                description: result.description,
+                users: result.users,
+            }).subscribe((data) => {
+                if (data && data.success) {
+                    this.loadCourse(this.course.id);
+                }
+            });
+        }, () => undefined);
     }
 }
