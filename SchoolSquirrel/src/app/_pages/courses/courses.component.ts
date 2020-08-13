5bf528afb8137363cb1937612c85af741a0a5b3c
Hannes Rüger <hannesrueger@gmx.de>
2020-08-13 13:11:32 +0200

    (web) (courses) new course page is now a modal

--- SchoolSquirrel/src/app/_pages/courses/courses.component.ts
+++ SchoolSquirrel/src/app/_pages/courses/courses.component.ts
@@ -1,8 +1,9 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnInit, ViewChild, ElementRef } from "@angular/core";
 import { ActivatedRoute, Router } from "@angular/router";
 import { User } from "../../_models/User";
 import { RemoteService } from "../../_services/remote.service";
 import { Course } from "../../_models/Course";
+import { NgbModal } from "@ng-bootstrap/ng-bootstrap";
 
 @Component({
     selector: "app-courses",
@@ -10,27 +11,35 @@ import { Course } from "../../_models/Course";
     styleUrls: ["./courses.component.scss"],
 })
 export class CoursesComponent implements OnInit {
-    public newCourse = false;
     public newCourseName = "";
     public newCourseUsers: User[] = [];
     public loading = false;
     public invalid = false;
     public courses: Course[] = [];
+    @ViewChild("content") private modal: ElementRef;
 
     constructor(
         private route: ActivatedRoute,
         private remoteService: RemoteService,
+        private modalService: NgbModal,
         private router: Router,
-    ) {
-        if (this.route.snapshot.url[this.route.snapshot.url.length - 1].path == "new") {
-            this.newCourse = true;
-        }
-    }
+    ) {}
 
     public ngOnInit(): void {
         this.remoteService.get("courses").subscribe((data) => {
             this.courses = data;
         });
+        if (this.route.snapshot.url[this.route.snapshot.url.length - 1].path == "new") {
+            setTimeout(() => {
+                this.modalService.open(this.modal).result.then(() => {
+                    if (!this.invalid) {
+                        this.createCourse();
+                    }
+                }, () => {
+                    this.router.navigate(["/courses"]);
+                });
+            });
+        }
     }
 
     public createCourse(): void {
@@ -42,6 +51,7 @@ export class CoursesComponent implements OnInit {
             this.loading = false;
             return;
         }
+        this.modalService.dismissAll();
         this.remoteService.post("courses", {
             name: this.newCourseName,
             users,
