0b96bb44ac78556c1472e1cbc464837592aa30d9
Hannes Rüger <hannesrueger@gmx.de>
2020-08-13 11:45:48 +0200

    (web) fixed modals not scrollable

--- SchoolSquirrel/src/app/app.component.ts
+++ SchoolSquirrel/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from "@angular/core";
 import { Router, NavigationEnd } from "@angular/router";
+import { NgbModalConfig, NgbModal } from "@ng-bootstrap/ng-bootstrap";
 import { isElectron } from "./_helpers/isElectron";
 import { PushService } from "./_services/push.service";
 import { NavbarUsersService } from "./_services/navbar-users.service";
@@ -9,6 +10,7 @@ import { AuthenticationService } from "./_services/authentication.service";
     selector: "app-root",
     templateUrl: "./app.component.html",
     styleUrls: ["./app.component.scss"],
+    providers: [NgbModalConfig, NgbModal],
 })
 export class AppComponent {
     isFullScreenPage: boolean;
@@ -17,7 +19,9 @@ export class AppComponent {
         private pushService: PushService,
         private navbarUsersService: NavbarUsersService,
         private authenticationService: AuthenticationService,
+        modalConfig: NgbModalConfig,
     ) {
+        modalConfig.scrollable = true;
         this.router.events.subscribe((r) => {
             if (r instanceof NavigationEnd) {
                 if (r.url.indexOf("login") == -1) {
