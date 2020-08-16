891735c5f81ff6a7aaf85655f64ef732b0ec5105
Hannes Rüger <hannesrueger@gmx.de>
2020-08-16 17:11:40 +0200

    (docker) fixed file outside build context

--- docker/build.sh
+++ docker/build.sh
@@ -4,7 +4,9 @@ npm run build -- --outputPath=../docker/frontend
 cd ../api
 npm install
 npx @zeit/ncc build src/index.ts -o ../docker/backend
-cd ../docker
+cd ..
+cp ./container-env.json ./docker/container-env.json
+cd ./docker
 PACKAGE_VERSION=$(cat ../SchoolSquirrel/package.json | grep version | head -1 | awk -F: '{ print $2 }' | sed 's/[",]//g' | tr -d '[[:space:]]')
 docker build -t schoolsquirrel/schoolsquirrel:v$PACKAGE_VERSION .
 rm -r frontend backend
