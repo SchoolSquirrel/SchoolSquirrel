9cd36efa3ae1c08201e2a967d7c3be998f09c7d6
H. Rüger <hannesrueger@gmx.de>
2020-08-25 10:02:50 +0200

    (web) (user settings) added ui

--- SchoolSquirrel/src/app/_pages/user-settings/user-settings.component.ts
+++ SchoolSquirrel/src/app/_pages/user-settings/user-settings.component.ts
@@ -1,4 +1,7 @@
 import { Component } from "@angular/core";
+import { isElectron } from "../../_helpers/isElectron";
+import { RemoteService } from "../../_services/remote.service";
+import { AuthenticationService } from "../../_services/authentication.service";
 
 @Component({
     selector: "app-user-settings",
@@ -6,5 +9,30 @@ import { Component } from "@angular/core";
     styleUrls: ["./user-settings.component.scss"],
 })
 export class UserSettingsComponent {
-    //
+    public isElectron: boolean = isElectron();
+    public settings = {
+        autostart: true,
+        startMinimized: false,
+    };
+    public privacySettings = [
+        {
+            title: "Lesebestätigungen",
+            description: "Anderen Personen im Chat anzeigen, ob Nachrichten gelesen wurden. Wenn Du diese Einstellung deaktivierst, kannst Du auch die Lesebestätigungen von anderen Personen nicht sehen.",
+            key: "deliveryStatus",
+        },
+        {
+            title: "Online-Status",
+            description: "Anderen Personen anzeigen, ob Du gerade online bist. Wenn Du diese Einstellung deaktivierst, kannst Du auch den Online-Status anderer Personen nicht sehen.",
+            key: "onlineStatus",
+        },
+        {
+            title: "Zuletzt Online",
+            description: "Anderen Personen anzeigen, wann Du zuletzt online warst. Wenn Du diese Einstellung deaktivierst, kannst Du auch den Zuletzt Online Zeitpunkt anderer Personen nicht sehen.",
+            key: "lastOnline",
+        },
+    ]
+    constructor(
+        public remoteService: RemoteService,
+        public authenticationService: AuthenticationService,
+    ) { }
 }
