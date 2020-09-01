cb8dd4c31fcd40cc8e398f33ce27c926d897b966
H. Rüger <hannesrueger@gmx.de>
2020-09-01 08:55:53 +0200

    (docs) (scripts) added api docs generation script

---
+++ docs/scripts/generate-api-docs.ts
@@ -0,0 +1,34 @@
+import * as swaggerJsdoc from "swagger-jsdoc";
+import * as fs from "fs";
+import * as path from "path";
+
+// Swagger set up
+const options = {
+    swaggerDefinition: {
+      swagger: "2.0",
+      info: {
+        title: "SchoolSquirrel API Documentation",
+        version: "1.0.0",
+        description:
+          "This is the documentation for the SchoolSquirrel API.",
+        license: {
+          name: "MIT",
+          url: "https://github.com/SchoolSquirrel/SchoolSquirrel/blob/master/LICENSE"
+        },
+        contact: {
+          name: "Hannes Rüger",
+          url: "https://github.com/hrueger"
+        }
+      },
+      servers: [
+        {
+          url: "http://localhost:3000/api/"
+        }
+      ]
+    },
+    apis: [
+        "../../api/src/controllers/*.ts",
+    ]
+};
+const specs = swaggerJsdoc(options);
+fs.writeFileSync(path.join(__dirname, "../docs/developers/swagger.json"), JSON.stringify(specs, undefined, 4));
