3b2c43f5c784f8c86c7a0dcc5c2f6391639ec4dc
Hannes Rüger <hannesrueger@gmx.de>
2020-09-08 17:35:33 +0200

    (web) (login) show password suggestions

--- SchoolSquirrel/src/app/_components/password-strength-meter/password-strength-meter.service.ts
+++ SchoolSquirrel/src/app/_components/password-strength-meter/password-strength-meter.service.ts
@@ -5,6 +5,15 @@ console.log(zxcvbn);
 
 @Injectable()
 export class PasswordStrengthMeterService {
+    private options = {
+        language: "de",
+        keyboard_layouts: {
+            german: {
+                layout: "^° 1! 2\" 3§ 4$ 5% 6& 7/ 8( 9) 0= ß? ´`\n qQ wW eE rR tT zZ uU iI oO pP üÜ +*\n  aA sS dD fF gG hH jJ kK lL öÖ äÄ #'\n   yY xX cC vV bB nN mM ,; .: -_",
+                slanted: true,
+            },
+        },
+    }
     /**
    *  this will return the password strength score in number
    *  0 - too guessable
@@ -16,7 +25,7 @@ export class PasswordStrengthMeterService {
    *  @param password - Password
    */
     score(password: string): number {
-        const result = zxcvbn(password);
+        const result = zxcvbn(password, this.options);
         return result.score;
     }
 
@@ -28,7 +37,7 @@ export class PasswordStrengthMeterService {
    */
     scoreWithFeedback(password: string):
     { score: number; feedback: { suggestions: string[]; warning: string } } {
-        const result = zxcvbn(password);
+        const result = zxcvbn(password, this.options);
         return { score: result.score, feedback: result.feedback };
     }
 }
