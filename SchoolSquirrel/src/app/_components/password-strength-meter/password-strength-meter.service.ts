25d07af217d82407901e2bdd84cc94cc5fcd0c46
Hannes Rüger <hannesrueger@gmx.de>
2020-09-20 13:21:57 +0200

    (web) removed unnecessary `console.log()`

--- SchoolSquirrel/src/app/_components/password-strength-meter/password-strength-meter.service.ts
+++ SchoolSquirrel/src/app/_components/password-strength-meter/password-strength-meter.service.ts
@@ -1,8 +1,6 @@
 import { Injectable } from "@angular/core";
 import { zxcvbn } from "zxcvbn3";
 
-console.log(zxcvbn);
-
 @Injectable()
 export class PasswordStrengthMeterService {
     private options = {
