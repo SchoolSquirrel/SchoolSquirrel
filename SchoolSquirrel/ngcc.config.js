f2d8a14fb708f47f091099c0b9c66ff5c00a940c
Hannes Rüger <hannesrueger@gmx.de>
2020-10-15 16:29:47 +0200

    (web) updated packages

--- SchoolSquirrel/ngcc.config.js
+++ SchoolSquirrel/ngcc.config.js
@@ -1,21 +1,5 @@
 module.exports = {
     packages: {
-        "@nativescript/angular": {
-            entryPoints: {
-                ".": {
-                    override: {
-                        main: "./index.js",
-                        typings: "./index.d.ts",
-                    },
-                    ignoreMissingDependencies: true,
-                },
-            },
-            ignorableDeepImportMatchers: [
-                /zone.js\//,
-                /tns-core-modules\//,
-                /@nativescript\/core\//,
-            ],
-        },
         "nativescript-localize": {
             entryPoints: {
                 ".": {
@@ -28,45 +12,6 @@ module.exports = {
             },
             ignorableDeepImportMatchers: [
                 /@nativescript\/core\//,
-            ],
-        },
-        "nativescript-datetimepicker": {
-            entryPoints: {
-                ".": {
-                    override: {
-                        main: "./index.js",
-                        typings: "./index.d.ts",
-                    },
-                    ignoreMissingDependencies: true,
-                },
-                angular: {
-                    override: {
-                        main: "./index.js",
-                        typings: "./index.d.ts",
-                    },
-                    ignoreMissingDependencies: true,
-                },
-            },
-            ignorableDeepImportMatchers: [
-                /tns-core-modules\//,
-                /@nativescript\/core\//,
-                /@nativescript\/angular\//,
-            ],
-        },
-        "nativescript-emoji-picker": {
-            entryPoints: {
-                angular: {
-                    override: {
-                        main: "./emoji-picker.directives.js",
-                        typings: "./emoji-picker.directives.d.ts",
-                    },
-                    ignoreMissingDependencies: true,
-                },
-            },
-            ignorableDeepImportMatchers: [
-                /tns-core-modules\//,
-                /@nativescript\/core\//,
-                /@nativescript\/angular\//,
             ],
         },
     },
